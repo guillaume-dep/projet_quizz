b82b9a5aff0a3a9d0d2c4da0427ba8b9926d8891
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-07 16:09:26 +0200

    feat: ajout handleCreate et handleJoin

--- server/controllers/ioController.js
+++ server/controllers/ioController.js
@@ -1,15 +1,62 @@
-import GameManager from "../gameManager.js";
+import GameManager from "../gameManager"
 
 /**
  * IOController class to manage the game connections 
  */
 export default class IOController {
     #io
-    #gameManager
+    #rooms
 
-    constructor(io, gameManager) {
+    constructor(io, rooms) {
         this.#io = io
-        this.#gameManager = gameManager;
+        this.#rooms = rooms /* Map<code, GameManager> */
+    }
+
+    registerSocket(socket) {
+        socket.on("create_game")
+        socket.on("join_game")
+        socket.on("start_game")
+        socket.on("submit_answer")
+        socket.on("request_next_question")
+    }
+
+    handleCreateGame(socket, player_data) {
+
+        const code = generateCode();
+        const gameManager = new GameManager()
+
+        gameManager.setHost(socket.id);
+        gameManager.addPlayer(socket.id, player_data.name, player_data.domain);
+        this.#rooms.set(code, gameManager)
+        socket.join(code)
+
+        socket.emit("game_created", { code }) /* To print out the code */
+    }
+
+    handleJoinGame(socket, player_data, code) {
+        const gameManager = this.#rooms.get(code);
+        if (!gameManager) { /* Code doesn't exist*/
+            socket.emit("error", { message: "Game not found ! Try another code." });
+            return;
+        }
+
+        gameManager.addPlayer(socket.id, player_data.name, player_data.domain);
+        socket.join(code);
+
+        /* Inform that a player joined */
+        this.io.to(code).emit("player_joined", { players: gameManager.players() });
+    }
+
+    handleStartGame() {
+
+    }
+
+    handleSubmitAnswer() {
+
+    }
+
+    handleNextQuestion() {
+
     }
 
 
