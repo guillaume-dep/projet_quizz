b2d01b632179e7a6094873cb03fb7b941d23f835
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-07 15:14:52 +0200

    feat: ajout constructeur

--- server/controllers/ioController.js
+++ server/controllers/ioController.js
@@ -1,3 +1,16 @@
+import GameManager from "../gameManager.js";
+
+/**
+ * IOController class to manage the game connections 
+ */
 export default class IOController {
+    #io
+    #gameManager
+
+    constructor(io, gameManager) {
+        this.#io = io
+        this.#gameManager = gameManager;
+    }
+
 
 }
