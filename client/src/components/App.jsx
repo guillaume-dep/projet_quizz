eb0467caa02843de8b71adf407920170daa1a679
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-08 11:28:38 +0200

    feat: ajout de VIEWS

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,10 @@
 import {socket} from "../socket/socket.js";
 import { SOCKET_EVENTS as SK } from "../../../shared/socketEvents.js";
+import { useState } from "react";
 
 const App = () => { 
+
+  const [gameView, setGameView] = useState()
   return <div></div>; 
 }; 
 
---
+++ client/src/components/utils/views.js
@@ -0,0 +1,13 @@
+export const VIEWS = {
+    /* Game creation */
+    HOME: "HOME",
+
+    /* Game waiting screen */
+    LOBBY: "LOBBY",
+
+    /* Game screen with the question */
+    GAME: "GAME",
+
+    /* Game result view */
+    RESULT: "RESULT"
+}
