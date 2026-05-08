ecbb4614bd4b8651dfc9d39c52de333d370e4911
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-08 10:09:56 +0200

    feat: ajout généralisation des socket events

---
+++ shared/socketEvents.js
@@ -0,0 +1,22 @@
+export const SOCKET_EVENTS = {
+    CONNECT: "connect",
+    DISCONNECT: "disconnect",
+
+    CREATE_GAME: "create_game",
+    JOIN_GAME: "join_game",
+
+    GAME_CREATED: "game_created",
+    PLAYER_JOINED: "player_joined",
+    PLAYER_REMOVED: "player_removed",
+
+    START_GAME: "start_game",
+    GAME_STARTED: "game_started",
+
+    NEW_QUESTION: "new_question",
+    SUBMIT_ANSWER: "submit_answer",
+
+    SHOW_RESULTS: "show_results",
+    GAME_OVER: "game_over",
+
+    ERROR: "error",
+};
