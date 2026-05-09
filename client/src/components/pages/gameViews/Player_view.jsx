8ee1b657aeb8affb8fbe517c0f4e7ca66a1a9a6f
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-09 16:21:59 +0200

    feat: ajout des logs

--- client/src/components/pages/gameViews/Player_view.jsx
+++ client/src/components/pages/gameViews/Player_view.jsx
@@ -3,12 +3,41 @@ import { SOCKET_EVENTS as SK } from "../../../../../shared/socketEvents.js";
 import { ROLE } from "../../../../../shared/utils/role.js";
 import { useState, useEffect } from "react";
 
-const Player_view = ({ question }) => {
+const Player_view = ({ question, gameCode, hasAnswered, setHasAnswered }) => {
     const { id, text, theme, answers, correctIndex, value, coef } = question;
+    const colors = ["red", "blue", "green", "yellow"];
+
+
+    /* Double verification so the client can't spam (there's also a server verification) */
+    const handleAnswer = (event) => {
+        if (hasAnswered) return;
+
+        setHasAnswered(true)
+
+        let answerIndex = Number(event.target.value)
+        console.log(`Envoie de la réponse depuis Player_view : ${answerIndex}`)
+        socket.emit(SK.SUBMIT_ANSWER, answerIndex, gameCode)
+    }
+
+    const renderButtons = () => {
+        return answers.map((answer, index) => (
+            <button
+                value={index}
+                key={index}
+                disabled={hasAnswered}
+                className={`answer-btn ${colors[index]}`}
+                onClick={handleAnswer}
+            >
+                {answer}
+            </button>
+        ));
+    }
+
     return (
         <div>
-            {theme}
             {text}
+            <br />
+            {renderButtons()}
         </div>
 
     )
