d7bdf9d6e1f38befd724974f4fe4f92bd32eacf9
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-09 16:21:43 +0200

    feat: ajout gestion réponse

--- client/src/components/pages/Game.jsx
+++ client/src/components/pages/Game.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, use } from "react";
 import { socket } from "../../socket/socket.js";
 import { SOCKET_EVENTS as SK } from "../../../../shared/socketEvents.js";
 import { ROLE } from "../../../../shared/utils/role.js";
@@ -6,12 +7,38 @@ import Player_view from "./gameViews/Player_view.jsx"
 
 const Game = ({ role, gameCode, scores, errorMessage, setErrorMessage, question }) => {
 
+    const [answer, setAnswer] = useState(null);
+    const [hasAnswered, setHasAnswered] = useState(false);
+
+    useEffect(() => {
+        const handleSubmittedAnswer = (answerFromServer) => {
+            console.log("Reponse reçu du serveur à la question :", answerFromServer);
+            setAnswer(answerFromServer)
+        }
+
+        socket.on(SK.SUBMITTED_ANSWER, handleSubmittedAnswer)
+
+        return () => {
+            socket.off(SK.SUBMITTED_ANSWER, handleSubmittedAnswer)
+        }
+    })
+
     return (
         <div>
             {(role === ROLE.HOST) ? (
-                <Host_view question={question} />
+                <Host_view
+                    question={question}
+                    gameCode={gameCode}
+                    hasAnswered={hasAnswered}
+                    setHasAnswered={setHasAnswered}
+                />
             ) :
-                <Player_view question={question} />
+                <Player_view
+                    question={question}
+                    gameCode={gameCode}
+                    hasAnswered={hasAnswered}
+                    setHasAnswered={setHasAnswered}
+                />
             }
         </div>
     )
