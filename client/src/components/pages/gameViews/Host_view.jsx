3fbcba1d2f8d446a85d4420d4877d752bf4f4363
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-11 13:09:38 +0200

    feat: changement pour afficher display questions

--- client/src/components/pages/gameViews/Host_view.jsx
+++ client/src/components/pages/gameViews/Host_view.jsx
@@ -1,23 +1,9 @@
+import QuestionDisplay from "../QuestionDisplay";
+
 const Host_view = ({ question, gameCode }) => {
-
-    const { id, text, theme, answers, correctIndex, value, coef } = question;
-    const colors = ["red", "blue", "green", "yellow"];
-
-    const renderAnswers = () => {
-        return answers.map((answer, index) => (
-            <div key={index} className={`answer ${colors[index]}`}>
-                {answer}
-            </div>
-        ));
-    }
-
     return (
         <div>
-            {theme}
-            <br />
-            {text}
-            <br />
-            {renderAnswers()}
+            <QuestionDisplay gameCode={gameCode} question={question} />
         </div>
 
     )
