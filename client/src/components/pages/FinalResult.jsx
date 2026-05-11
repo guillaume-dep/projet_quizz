5d36a30bb1d3e81ad5bd66db8fb580bfd190e9fe
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-11 19:11:32 +0200

    feat: ajout écran résultat finaux

--- client/src/components/pages/FinalResult.jsx
+++ client/src/components/pages/FinalResult.jsx
@@ -1,6 +1,23 @@
-const FinalResult = () => {
+
+import { ROLE } from "../../../../shared/utils/role.js";
+
+const FinalResult = ({ scores }) => {
+
+    const renderFinalResult = scores
+        .filter(player => player.domain !== "")
+        .map(({ name, score, domain }, index) => (
+            <div key={index}>
+                <p>Name : {name}</p>
+                <p>score : {score}</p>
+                <p>domain : {domain}</p>
+            </div>
+        ))
+
     return (
-        <div>Final results</div>
+        <div className="FinalResult">
+            Final results :
+            {renderFinalResult}
+        </div>
     )
 }
 
