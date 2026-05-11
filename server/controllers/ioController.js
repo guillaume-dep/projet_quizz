9d4d1194d35a101e0160517991ed8af661153445
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-11 18:33:50 +0200

    feat: GAME MANAGER AVEC DES VALEURS EN DUR - A SUPPRIMER

--- server/controllers/ioController.js
+++ server/controllers/ioController.js
@@ -219,6 +219,7 @@ export default class IOController {
         console.log("Envoie des résultats de la question, partie : ", code)
         this.#io.to(code).emit(SK.SHOWN_RESULTS, {
             playerScore: gameManager.getScores(), /* [{name, score, domain} */
+            isLastQuestion: gameManager.isLastQuestion()
         })
     }
 
