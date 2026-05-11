ef76b8adf8e7c57e6ea752edf22b54b146dd0381
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-11 18:07:38 +0200

    feat: ajout result

--- client/src/components/pages/Result.jsx
+++ client/src/components/pages/Result.jsx
@@ -1,11 +1,27 @@
 import QuestionDisplay from "./QuestionDisplay";
-import ROLE from "../../../../shared/utils/role";
-import 
+import ResultHostView from "./resultViews/ResultHostView";
+import ResultPlayerView from "./resultViews/ResultPlayerView";
+import { useState, useEffect } from "react";
+import { socket } from "../../socket/socket.js";
+import { SOCKET_EVENTS as SK } from "../../../../shared/socketEvents.js";
+import { ROLE } from "../../../../shared/utils/role.js";
 
-const Result = (gameCode, scores) => {
+const Result = ({ role, question, gameCode, answer }) => {
+
+    const renderView = () => {
+        if (role === ROLE.HOST) {
+            console.log("Affichage résultat du host")
+            return <ResultHostView question={question} gameCode={gameCode} />
+        }
+
+        console.log("Affichage résultat du player")
+        return <ResultPlayerView question={question} gameCode={gameCode} answer={answer} />
+    }
 
     return (
-        <div>Result screen </div>
+        <div className="Result">
+            {renderView()}
+        </div>
     )
 }
 
