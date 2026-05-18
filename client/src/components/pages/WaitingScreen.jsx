922675acd1b8b9ac35950b0cb3389f4de0db7615
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-18 19:53:44 +0200

    feat: ajout waiting screen + waiting view

--- client/src/components/pages/WaitingScreen.jsx
+++ client/src/components/pages/WaitingScreen.jsx
@@ -1,5 +1,21 @@
+/* --- CSS --- */
+import styles from "../../style/waitingScreen.module.css"
+
 const WaitingScreen = () => {
-    return <div className="waitingScreen">WaitingScreen</div>
+    return (
+        <div className={styles.waitingScreen}>
+
+            <div className={styles.wrapper}>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.circle}></div>
+                <div className={styles.shadow}></div>
+                <div className={styles.shadow}></div>
+                <div className={styles.shadow}></div>
+            </div>
+            <span className={styles.waitingTitle}>waiting for other players to answer</span>
+        </div>
+    )
 }
 
 export default WaitingScreen
