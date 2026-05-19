67591f127fca643b28c654597f01f888cd9dd294
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-05-19 18:18:06 +0200

    feat: ajout questions

--- server/data/questions.js
+++ server/data/questions.js
@@ -2,6 +2,7 @@ import { DIFFICULTY } from "../../client/src/components/utils/difficulty.js";
 import { STUDY_DOMAIN } from "../utils/studyDomain.js";
 
 const questions = [
+    /* ---------- EASY ---------- */
     {
         id: 0,
         text: "Quel est le meilleur langage pour programmer ?",
@@ -33,5 +34,82 @@ const questions = [
         value: 1,
         coef: 1,
         difficulty: DIFFICULTY.EASY
+    },
+    {
+        id: 1,
+        text: "Quel est le meilleur langage pour débuter ?",
+        theme: STUDY_DOMAIN.INFORMATIQUE,
+        answers: [
+            "Python",
+            "C",
+            "Kotlin",
+            "Haskell"
+        ],
+        correctIndex: 0,
+        value: 1,
+        coef: 1,
+        difficulty: DIFFICULTY.EASY
+    },
+    {
+        id: 1,
+        text: "Quel est le meilleur langage pour débuter ?",
+        theme: STUDY_DOMAIN.INFORMATIQUE,
+        answers: [
+            "Python",
+            "C",
+            "Kotlin",
+            "Haskell"
+        ],
+        correctIndex: 0,
+        value: 1,
+        coef: 1,
+        difficulty: DIFFICULTY.EASY
+    },
+    {
+        id: 1,
+        text: "Quel est le meilleur langage pour débuter ?",
+        theme: STUDY_DOMAIN.INFORMATIQUE,
+        answers: [
+            "Python",
+            "C",
+            "Kotlin",
+            "Haskell"
+        ],
+        correctIndex: 0,
+        value: 1,
+        coef: 1,
+        difficulty: DIFFICULTY.EASY
+    },
+    {
+        id: 1,
+        text: "Quel est le meilleur langage pour débuter ?",
+        theme: STUDY_DOMAIN.INFORMATIQUE,
+        answers: [
+            "Python",
+            "C",
+            "Kotlin",
+            "Haskell"
+        ],
+        correctIndex: 0,
+        value: 1,
+        coef: 1,
+        difficulty: DIFFICULTY.EASY
+    },
+    {
+        id: 1,
+        text: "Quel est le meilleur langage pour débuter ?",
+        theme: STUDY_DOMAIN.INFORMATIQUE,
+        answers: [
+            "Python",
+            "C",
+            "Kotlin",
+            "Haskell"
+        ],
+        correctIndex: 0,
+        value: 1,
+        coef: 1,
+        difficulty: DIFFICULTY.EASY
     }
 ]
+
+export default questions;
