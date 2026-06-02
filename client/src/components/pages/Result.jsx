5fd3a0c9d75f19cc8a7c0dd29c7a592358032ed6
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-06-02 18:47:08 +0200

    feat: clean code 2

--- client/src/components/pages/Result.jsx
+++ client/src/components/pages/Result.jsx
@@ -1,13 +1,7 @@
-import QuestionDisplay from "./QuestionDisplay";
 import ResultHostView from "./resultViews/ResultHostView";
 import ResultPlayerView from "./resultViews/ResultPlayerView";
-import { useState, useEffect } from "react";
-import { socket } from "../../socket/socket.js";
-import { SOCKET_EVENTS as SK } from "../../../../shared/socketEvents.js";
 import { ROLE } from "../../../../shared/utils/role.js";
-import { AnimatePresence, motion } from "framer-motion"
-import { VIEWS } from "../utils/views.js";
-
+import { motion } from "framer-motion"
 
 const Result = ({ role,
     question,
